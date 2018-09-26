4c8a7e9201905d2b84aef80b5cc59a2fa98d655c
DAIZHENWEI <32122197+DAIZHENWEI@users.noreply.github.com>
2018-09-26 16:20:05 -0500

    Update BDMMA.R

--- R/BDMMA.R
+++ R/BDMMA.R
@@ -27,7 +27,7 @@
 #' Y <- dat$Y
 #' batch <- dat$batch
 #' continuous <- dat$continuous
-#' (not run)
+#' ## (not run)
 #' ## output <- BDMMA(X, Y, batch, continuous, burn_in = 3000, sample_period = 3000)
 #' @export
 
@@ -289,7 +289,7 @@ fdr_cut = function(PIP_vec, alpha = 0.1){
 #' Y <- dat$Y
 #' batch <- dat$batch
 #' continuous <- dat$continuous
-#' (not run)
+#' ## (not run)
 #' ## output <- BDMMA(X, Y, batch, continuous, burn_in = 3000, sample_period = 3000)
 #' ## figure <- trace_plot(output$trace, param = c("alpha_1", "beta1_10"))
 #' ## print(figure)
